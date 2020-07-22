0899af4d8076de1d5c319456f1509fe607995926
zephol <joansalograu@gmail.com>
2020-07-22 13:57:46 +0200

    afegit codi a membrane_reuse_option

--- survey_1.js
+++ survey_1.js
@@ -16,21 +16,21 @@ let app = new Vue({
     ],
 
     membrane_reuse_options:[
-      {name:"Landfill disposal or incineration",                                                                color:"#be514e"},
-      {name:"Indirect recycling",                                                                               color:"#604b79"},
-      {name:"Membranes are suitable for an alternative management to landfill disposal or incineration.",       color:"#9ab959"},
-      {name:"Membranes are suitable for an alternative management to landfill disposal or incineration. " +
+      {code:"LI", name:"Landfill disposal or incineration",                                                                color:"#be514e"},
+      {code:"IR", name:"Indirect recycling",                                                                               color:"#604b79"},
+      {code:"AM", name:"Membranes are suitable for an alternative management to landfill disposal or incineration.",       color:"#9ab959"},
+      {code:"AMR", name:"Membranes are suitable for an alternative management to landfill disposal or incineration. " +
           "Membranes might be need to be rehydrated using 50% w/w ethanol during 15 min.",                      color:"#c2d59a"},
-      {name:"Membranes are suitable for an alternative management to landfill disposal or incineration. " +
+      {code:"AMS", name:"Membranes are suitable for an alternative management to landfill disposal or incineration. " +
           "Please fill the following survey to estimate which alternative would suit better.",                  color:"#9ab959"},
 
 
-      {name:"Intensive cleaning before considering an alternative management to landfill disposal " +
+      {code:"IC", name:"Intensive cleaning before considering an alternative management to landfill disposal " +
           "or incineration",                                                                                    color:"#0cae51"},
-      {name:"Not enough information. Potentially, membranes are suitable for alternative management.",          color:"#4dabc5"},
-      {name:"Not enough information. Potentially, apply intensive cleaning before alternative management.",     color:"#35849b"},
-      {name:"Not enough information. Potentially, indirect recycling.",                                         color:"#ffff00"},
-      {name:"Indirect recycling of those membranes placed in the second stage. Potentially, " +
+      {code:"NEIM", name:"Not enough information. Potentially, membranes are suitable for alternative management.",          color:"#4dabc5"},
+      {code:"NEIC", name:"Not enough information. Potentially, apply intensive cleaning before alternative management.",     color:"#35849b"},
+      {code:"NEIR", name:"Not enough information. Potentially, indirect recycling.",                                         color:"#ffff00"},
+      {code:"IRC", name:"Indirect recycling of those membranes placed in the second stage. Potentially, " +
           "apply intensive cleaning before considering an alternative management for those membranes " +
           "placed in the first stage.",                                                                         color:"#f79447"},
     ],
@@ -39,7 +39,18 @@ let app = new Vue({
     get_question_by_code(code){
       return this.questions.find(q=>q.code==code);
     },
+    get_membrane_reuse_option_by_code(code){
+      if(code){
+        let option = this.membrane_reuse_options.find(o=>o.code==code);
+        return option.name;
+      }
+      return "answer survey 1 to get a membrane reuse";
 
+    },
+    show_membrane_reuse(){
+      let code = this.get_membrane_reuse();
+      return this.get_membrane_reuse_option_by_code(code);
+    },
     is_rendered(question){
       let code = question.code;
 
@@ -75,23 +86,23 @@ let app = new Vue({
       }
 
       //Type of membrane
-      if(get_question("T").value == "Other")                       return "Landfill disposal or incineration";
-      if(get_question("T").value == "Ultrafiltration")             return "Landfill disposal or incineration";
-      if(get_question("T").value == "Microfiltration")             return "Landfill disposal or incineration";
+      if(get_question("T").value == "Other")                       return "LI";
+      if(get_question("T").value == "Ultrafiltration")             return "LI";
+      if(get_question("T").value == "Microfiltration")             return "LI";
 
       //Membrane configuration
-      if(get_question("C").value == "Other")                       return "Landfill disposal or incineration";
+      if(get_question("C").value == "Other")                       return "LI";
 
       //Membrane size
-      if(get_question("S").value == "Other")                       return "Landfill disposal or incineration";
+      if(get_question("S").value == "Other")                       return "LI";
 
       //Membrane weight
-      if(get_question("W").value == ">25kg")                       return "Landfill disposal or incineration";
+      if(get_question("W").value == ">25kg")                       return "LI";
 
       if((get_question("T").value == "Reverse osmosis" || get_question("T").value == "Nanofiltration") && get_question("C").value == "Spiral-wound" && get_question("S").value == "Length: 1m. Diameter: 0.2m" && (get_question("W").value == "<17kg" || get_question("W").value == "17-25 kg")){
         //External damage
         if(get_question("ED").value == "Yes"){
-          return "Indirect recycling";
+          return "IR";
         }else if (get_question("ED").value == "No") {
 
           //Fouling
@@ -101,16 +112,16 @@ let app = new Vue({
             if (get_question("ST").value == "Immersed in a water solution" || get_question("ST").value == "Wet") {
 
               //Weight
-              if (get_question("W").value == "17-25 kg") return "Indirect recycling";
-              else if (get_question("W").value == "<17kg") return "Intensive cleaning before considering an alternative management to landfill disposal or incineration";
-            } else if (get_question("ST").value == "Dry") return "Indirect recycling";
+              if (get_question("W").value == "17-25 kg") return "IR";
+              else if (get_question("W").value == "<17kg") return "IC";
+            } else if (get_question("ST").value == "Dry") return "IR";
           } else if (get_question("F").value == "Other") {
 
             //Membrane storage
             if (get_question("ST").value == "Immersed in a water solution" || get_question("ST").value == "Wet") {
               //Weight
-              if (get_question("W").value == "17-25 kg") return "Intensive cleaning before considering an alternative management to landfill disposal or incineration";
-              else if (get_question("W").value == "<17kg") return "Membranes are suitable for an alternative management to landfill disposal or incineration. Please fill the following survey to estimate which alternative would suit better.";
+              if (get_question("W").value == "17-25 kg") return "IC";
+              else if (get_question("W").value == "<17kg") return "AMS";
 
             } else if (get_question("ST").value == "Dry") {
 
@@ -121,33 +132,33 @@ let app = new Vue({
                 if (get_question("W").value == "17-25 kg") {
                   if (get_question("P").value == "Mix" && (get_question("RP").value == "Operating more than the expected lifespan" || get_question("RP").value == "Lost of membrane integrity" || get_question("RP").value == "Don't know")) {
                     //2*
-                    return "Indirect recycling of those membranes placed in the second stage. Potentially, apply intensive cleaning before considering an alternative management for those membranes placed in the first stage.";
+                    return "IRC";
                   } else if (get_question("P").value == "Single pass" || get_question("P").value == "Double pass - single stage" || get_question("P").value == "Mix") {
                     //1*
-                    return "Intensive cleaning before considering an alternative management to landfill disposal or incineration";
+                    return "IC";
                   } else if (get_question("P").value == "Double pass - double stage") {
                     //3*
-                    return "Indirect recycling";
+                    return "IR";
                   }
-                } else if (get_question("W").value == "<17kg") return "Membranes are suitable for an alternative management to landfill disposal or incineration. Membranes might be need to be rehydrated using 50% w/w ethanol during 15 min.";
+                } else if (get_question("W").value == "<17kg") return "AMR";
               } else if (get_question("D").value == "<1 month") {
 
                 //Weight
-                if (get_question("W").value == "17-25 kg") return "Intensive cleaning before considering an alternative management to landfill disposal or incineration";
-                else if (get_question("W").value == "<17kg") return "Membranes are suitable for an alternative management to landfill disposal or incineration. Please fill the following survey to estimate which alternative would suit better.";
+                if (get_question("W").value == "17-25 kg") return "IC";
+                else if (get_question("W").value == "<17kg") return "AMS";
               }
             }
           }
         }
       }
 
-      return "answer survey 1 to get a membrane reuse";
+      return "";
     },
 
     //frontend
     get_membrane_reuse_color(){
-      let name  = this.get_membrane_reuse();
-      let reuse = this.membrane_reuse_options.find(o=>o.name==name);
+      let code  = this.get_membrane_reuse();
+      let reuse = this.membrane_reuse_options.find(o=>o.code==code);
       if(reuse) return reuse.color;
       else return "";
     },
