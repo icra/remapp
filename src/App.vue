cd8ac1cfbe729ca2961c8841f5ae5177981f6c29
zephol <joansalograu@gmail.com>
2020-07-23 12:31:39 +0200

    migrate to vue components

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  Menus
+</template>
+
+<script>
+  import Menus from "./components/Menus";
+
+  export default {
+    name: 'app',
+    components: {
+      Menus
+    },
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style>
+
+
+</style>
