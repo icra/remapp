0d1febaed45d1aed23f124ab2466a891ee2e6309
roserbrugues <roserbrugues@gmail.com>
2020-07-29 12:06:36 +0200

    header & footer added

---
+++ src/components/CaseStudies.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "CaseStudies"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/FactSheets.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: "FactSheets"
+  }
+</script>
+
+<style scoped>
+
+</style>
